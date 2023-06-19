455309fc07f2421d19b85f82f9cecf8f647ead88
Fredrik <fredrikjp-98@hotmail.com>
2023-06-19 06:03:55 +0200

    Attmepted gradient descent algorithm. Not working as expected

--- Project1/code/VMC.py
+++ Project1/code/VMC.py
@@ -1,5 +1,5 @@
 import numpy as np
-from random import random, seed
+from random import random, seed, normalvariate
 import time
 from numba import int32, float64
 from numba.experimental import jitclass
@@ -16,7 +16,8 @@ spec = [
     ("om_z", float64),
 ]
 
-#@jitclass(spec)
+
+# @jitclass(spec)
 class VMC:
     def __init__(self, N_particles, alpha, beta, a):
         self.alpha = alpha
@@ -39,10 +40,10 @@ class VMC:
             g *= np.exp(-self.alpha * np.linalg.norm(r[i, :]) ** 2)
         return g
 
-    def QuantumForce(self, r, alpha, beta):
+    def QuantumForce(self, r):
         if r.shape[1] == 3:
-            r[:, 2] *= np.sqrt(beta)
-        qforce = -4 * alpha * np.sum(r, axis=0)
+            r[:, 2] *= np.sqrt(self.beta)
+        qforce = -4 * self.alpha * r
         return qforce
 
     def V_ext(self, r):
@@ -65,13 +66,15 @@ class VMC:
             for j in range(i + 1, self.N_particles):
                 r_ij = np.linalg.norm(r[i, :] - r[j, :])
                 if r_ij < self.a:
-                    V += np.inf
+                    V += 1e10
         return V
 
     def local_energy(self, r, dim):
         laplacian_sum = 0
         if self.N_particles == 1:
-            laplacian_sum += 4*self.alpha**2*np.sum(r**2) - 6*self.alpha
+            laplacian_sum += (
+                4 * self.alpha**2 * np.sum(r**2) - 6 * self.alpha
+            )
             # Local energy
             EL = (
                 -self.h_bar**2 / (2 * self.m) * laplacian_sum
@@ -80,7 +83,7 @@ class VMC:
             )
             return EL
 
-        if False:#dim == 1 or dim == 2:
+        if False: #dim == 1 or dim == 2:
             alpha = self.alpha
             a = self.a
             # sum of laplacian_k acting on Psi_T and divided by Psi_T
@@ -96,7 +99,8 @@ class VMC:
                             @ (r[k, :] - r[l, :])
                             * a
                             / (r_kl**3 - a * r_kl**2)
-                            + (a**2 - 2 * a * r_kl) / (r_kl**2 - a * r_kl) ** 2
+                            + (a**2 - 2 * a * r_kl)
+                            / (r_kl**2 - a * r_kl) ** 2
                             + 2 * a / (r_kl**3 - a * r_kl**2)
                         )
                         for j in range(self.N_particles):
@@ -109,13 +113,14 @@ class VMC:
                                     / (
                                         (r_kl**2 - a * r_kl)
                                         * (r_kj**2 - a * r_kj)
-                                        * (r_kl * r_kj))
+                                        * (r_kl * r_kj)
+                                    )
                                 )
                 laplacian_sum += laplacian_term
 
         else:
             #######################################################################
-            
+
             alpha = self.alpha
             a = self.a
             N = self.N_particles
@@ -127,13 +132,18 @@ class VMC:
             # Remove diagonal elements as these are not used in the
             # calculation of the laplacian term
             r_diff = np.delete(
-                r_diff.reshape(N**2, dim), np.arange(N**2, step=N + 1), axis=0
+                r_diff.reshape(N**2, dim),
+                np.arange(N**2, step=N + 1),
+                axis=0,
             ).reshape(N, N - 1, dim)
             r_norm = np.delete(
                 r_norm.reshape(N**2), np.arange(N**2, step=N + 1), axis=0
             ).reshape(N, N - 1)
-            
-            
+
+            # Store as class variables for use in other methods 
+            self.r_diff = r_diff
+            self.r_norm = r_norm
+
             # Calculate laplacian sum
             # breakpoint()
 
@@ -143,19 +153,22 @@ class VMC:
                 - 4
                 * alpha
                 * a
-                * np.sum(r
-                @ np.sum(r_diff, axis=(1)).T, axis=1)
+                * np.sum(r @ np.sum(r_diff, axis=(1)).T, axis=1)
                 / np.sum(r_norm**3 - a * r_norm**2, axis=-1)
-                + np.sum((a**2 - 2 * a * r_norm) / (r_norm**2 - a * r_norm)**2
-                         + 2 * a / (r_norm**3 - a * r_norm**2), axis=-1)
-                #+ (a**2 * np.einsum("ijk,ik->i", r_diff, np.sum(r_diff, axis=1)))
-
-            ) 
-            for i in range(N-1):
+                + np.sum(
+                    (a**2 - 2 * a * r_norm) / (r_norm**2 - a * r_norm) ** 2
+                    + 2 * a / (r_norm**3 - a * r_norm**2),
+                    axis=-1,
+                )
+                # + (a**2 * np.einsum("ijk,ik->i", r_diff, np.sum(r_diff, axis=1)))
+            )
+            for i in range(N - 1):
                 laplacian_sum += (
                     a**2
                     * np.einsum(
-                        "jk,k->j", r_diff[:, i, :], np.sum(r_diff[:, i, :], axis=0)
+                        "jk,k->j",
+                        r_diff[:, i, :],
+                        np.sum(r_diff[:, i, :], axis=0),
                     )
                     / np.sum(
                         (r_norm[i, :] ** 2 - a * r_norm[i, :])
@@ -175,12 +188,39 @@ class VMC:
         )
         return EL
 
+    def SGD(self, r, lr):
+        if r.shape[0] == 1:
+            for i in range(1000):
+                grad_EL = -self.h_bar**2 / (2 * self.m) * (
+                    8 * self.alpha * np.sum(r**2) - 6 
+                )
+                self.alpha -= lr * grad_EL
+        else:
+            for i in range(1000):
+                grad_EL = -self.h_bar**2 / (2 * self.m) * np.sum(
+                8 * self.alpha * np.einsum("ij,ij->i", r, r) - 6 
+                - 4 * self.a * np.sum(r @ np.sum(self.r_diff, axis=(1)).T, axis=1)
+                / np.sum(self.r_norm**3 - self.a * self.r_norm**2, axis=-1)
+                )
+                print(grad_EL)
+                print(self.alpha)
+                self.alpha += lr * grad_EL 
+
+
+        
     def MC_Sampling(
-        self, N_cycles, StepSize, MaxVariations, Dimension, KE=False,
-        Importance_sampling = False
+        self,
+        N_cycles,
+        StepSize,
+        MaxVariations,
+        Dimension,
+        KE=False,
+        Importance_sampling=False,
+        lr = -1,
     ):
-        alpha = self.alpha
         alpha_values = np.zeros(MaxVariations)
+        beta = self.beta
+
         analytic_local_energies = np.zeros(MaxVariations)
         local_energies = np.zeros(MaxVariations)
         Variances = np.zeros(MaxVariations)
@@ -191,9 +231,14 @@ class VMC:
         PositionOld = np.zeros((self.N_particles, Dimension), np.double)
         PositionNew = np.zeros((self.N_particles, Dimension), np.double)
 
+        # Parameters in the Fokker-Planck simulation of the quantum force
+        D = 0.5
+        QuantumForceOld = np.zeros((self.N_particles, Dimension), np.double)
+        QuantumForceNew = np.zeros((self.N_particles, Dimension), np.double)
+
         for ia in range(MaxVariations):
             print(f"{ia*100/MaxVariations}%")
-            alpha_values[ia] = alpha
+            alpha_values[ia] = self.alpha
             energy = 0.0
             energy2 = 0.0
             kinetic_energy = 0.0
@@ -202,25 +247,68 @@ class VMC:
                 for j in range(Dimension):
                     PositionOld[i, j] = StepSize * (random() - 0.5)
             wfold = self.wavefunction(PositionOld)
+            if Importance_sampling:
+                QuantumForceOld = self.QuantumForce(PositionOld)
 
             time_EL = 0
             time_KE = 0
-
             analytic_local_energies[ia] = self.analytic_solution(PositionOld)
             for cycle in range(N_cycles):
                 for i in range(self.N_particles):
                     for j in range(Dimension):
-                        # New position
-                        PositionNew[i, j] = PositionOld[i, j] + StepSize * (
-                            random() - 0.5
-                        )
+                        if Importance_sampling:
+                            PositionNew[i, j] = (
+                                PositionOld[i, j]
+                                + normalvariate(0.0, 1.0) * np.sqrt(StepSize)
+                                + D * StepSize * QuantumForceOld[i, j]
+                            )
+                            wfnew = self.wavefunction(PositionNew)
+                            QuantumForceNew = self.QuantumForce(
+                                PositionNew
+                            )
+                            GreensFunction = 0.0
+                            for j in range(Dimension):
+                                GreensFunction += (
+                                    0.5
+                                    * (
+                                        QuantumForceOld[i, j]
+                                        + QuantumForceNew[i, j]
+                                    )
+                                    * (
+                                        D
+                                        * StepSize
+                                        * 0.5
+                                        * (
+                                            QuantumForceOld[i, j]
+                                            - QuantumForceNew[i, j]
+                                        )
+                                        - PositionNew[i, j]
+                                        + PositionOld[i, j]
+                                    )
+                                )
+                            GreensFunction = np.exp(GreensFunction)
+                            ProbabilityRatio = (
+                                GreensFunction * wfnew**2 / wfold**2
+                            )
+                            if random() <= ProbabilityRatio:
+                                for j in range(Dimension):
+                                    PositionOld[i, j] = PositionNew[i, j]
+                                    QuantumForceOld[i, j] = QuantumForceNew[
+                                        i, j
+                                    ]
+                                wfold = wfnew
 
-                    wfnew = self.wavefunction(PositionNew)
-                    # Metropolis test
-                    if random() <= wfnew**2 / wfold**2:
-                        for j in range(Dimension):
-                            PositionOld[i, j] = PositionNew[i, j]
-                        wfold = wfnew
+                        else:
+                            PositionNew[i, j] = PositionOld[i, j] + StepSize * (
+                                random() - 0.5
+                            )
+
+                            wfnew = self.wavefunction(PositionNew)
+                            # Metropolis test
+                            if random() <= wfnew**2 / wfold**2:
+                                for j in range(Dimension):
+                                    PositionOld[i, j] = PositionNew[i, j]
+                                wfold = wfnew
                 start_time_EL = time.time()
                 EL = self.local_energy(PositionOld, Dimension)
                 end_time_EL = time.time()
@@ -247,125 +335,11 @@ class VMC:
 
             avg_time_EL[ia] = time_EL / N_cycles
             avg_time_KE[ia] = time_KE / N_cycles
-
-            alpha += 0.1
-            self.alpha = alpha
-
-        Energies = {
-            "local_energy": local_energies,
-            "kinetic_energy": Kinetic_energies,
-        }
-        time_stats = {
-            "avarage_time_EL": avg_time_EL,
-            "avarage_time_KE": avg_time_KE,
-        }
-        return (
-            alpha_values,
-            analytic_local_energies,
-            Energies,
-            Variances,
-            time_stats,
-        )
-
-     
-
-
-    def MC_Sampling_IS(
-        self, N_cycles, StepSize, MaxVariations, Dimension, KE=False
-    ):
-        alpha = self.alpha
-        alpha_values = np.zeros(MaxVariations)
-        beta = self.beta
-        beta_values = np.zeros(MaxVariations)
-        analytic_local_energies = np.zeros(MaxVariations)
-        local_energies = np.zeros(MaxVariations)
-        Variances = np.zeros(MaxVariations)
-        Kinetic_energies = np.zeros(MaxVariations)
-        avg_time_EL = np.zeros(MaxVariations)
-        avg_time_KE = np.zeros(MaxVariations)
-
-        PositionOld = np.zeros((self.N_particles, Dimension), np.double)
-        PositionNew = np.zeros((self.N_particles, Dimension), np.double)
-
-        D = 0.5
-
-        QuantumForceOld = np.zeros((self.N_particles, Dimension), np.double)
-        QuantumForceNew = np.zeros((self.N_particles, Dimension), np.double)
-
-        
-        for ia in range(MaxVariations):
-            print(f"{ia*100/MaxVariations}%")
-            alpha_values[ia] = alpha
-            beta = 0.3
-            for jb in range(MaxVariations):
-                beta += .025
-                BetaValues[jb] = beta
-                energy = 0.0
-                energy2 = 0.0
-                kinetic_energy = 0.
-                DeltaE = 0.0
-                # Initial position
-                for i in range(self.N_particles):
-                    for j in range(Dimension):
-                        PositionOld[i, j] = StepSize * (random() - 0.5)
-                
-                wfold = self.wavefunction(PositionOld)
-                QuantumForceOld = QuantumForce(PositionOld,alpha, beta)
-
-
-
-                time_EL = 0
-                time_KE = 0
-
-                analytic_local_energies[ia] = self.analytic_solution(PositionOld)
-                for cycle in range(N_cycles):
-                    for i in range(self.N_particles):
-                        for j in range(Dimension):
-                            # New position
-                            PositionNew[i, j] = PositionOld[i, j] + StepSize * (
-                                random() - 0.5) + QuantumForceOld[i,j]*StepSize*D
-                        wfnew = self.wavefunction(PositionNew)
-                        QuantumForceNew = QuantumForce(PositionNew,alpha, beta)
-                        GreensFunction = 0.0
-
-                        for j in range(Dimension):
-                            GreensFunction += 0.5*(QuantumForceOld[i,j]+QuantumForceNew[i,j])*(D*StepSize*0.5*(QuantumForceOld[i,j]-QuantumForceNew[i,j])-PositionNew[i,j]+PositionOld[i,j])
-                        GreensFunction = exp(GreensFunction)
-                        # Metropolis test
-                        if random() <= GreensFunction*wfnew**2 / wfold**2:
-                            for j in range(Dimension):
-                                PositionOld[i, j] = PositionNew[i, j]
-                                QuantumForceOld[i,j] = QuantumForceNew[i,j]
-                            wfold = wfnew
-
-                    start_time_EL = time.time()
-                    EL = self.local_energy(PositionOld)
-                    end_time_EL = time.time()
-                    time_EL += end_time_EL - start_time_EL
-
-                    energy += EL
-                    energy2 += EL**2
-
-                    if KE:
-                        start_time_KE = time.time()
-                        kinetic_energy += self.kinetic_energy(PositionOld)
-                        end_time_KE = time.time()
-                        time_KE += end_time_KE - start_time_KE
-
-            # Calculate mean, variance and error
-            energy /= N_cycles
-            energy2 /= N_cycles
-            variance = energy2 - energy**2
-            error = np.sqrt(variance / N_cycles)
-            local_energies[ia] = energy
-            Variances[ia] = variance
-            kinetic_energy /= N_cycles
-            Kinetic_energies[ia] = kinetic_energy
-            avg_time_EL[ia] = time_EL / N_cycles
-            avg_time_KE[ia] = time_KE / N_cycles
-
-            alpha += 0.05
-            self.alpha = alpha
+            
+            if lr > 0:
+                self.SGD(PositionNew, lr)
+            else:
+                self.alpha += 0.1
 
         Energies = {
             "local_energy": local_energies,
@@ -382,8 +356,6 @@ class VMC:
             Variances,
             time_stats,
         )
-
-
 
     def kinetic_energy(self, r, h=0.001):
         # Calculate the laplace operator acting on the wavefunction numerically
@@ -419,10 +391,16 @@ class VMC:
             EL = (
                 -2
                 * self.h_bar**2
-                * 1 / 4
-                * self.a_ho**4 / self.m
+                * 1
+                / 4
+                * self.a_ho**4
+                / self.m
                 * np.sum(r * r)
-                + 3 * self.h_bar**2 / (2*self.m) * self.a_ho**2 * self.N_particles
+                + 3
+                * self.h_bar**2
+                / (2 * self.m)
+                * self.a_ho**2
+                * self.N_particles
                 + np.sum(self.V_ext(r))
             )
         else:
@@ -432,13 +410,13 @@ class VMC:
 
 if __name__ == "__main__":
     alpha = 0.1
-    N_particles = 1
+    N_particles = 10
     beta = 1
     a = 0  # No interaction
     N_cycles = 100
-    StepSize = .1
+    StepSize = 0.1
     MaxVariations = 10
-    Dimension = 2
+    Dimension = 3
     VMC_obj = VMC(N_particles, alpha, beta, a)
     (
         alpha_values,
@@ -448,6 +426,27 @@ if __name__ == "__main__":
         time_stats,
     ) = VMC_obj.MC_Sampling(
         N_cycles, StepSize, MaxVariations, Dimension, KE=True
+    )
+    print("alpha_values: ", alpha_values)
+    print("analytic_local_energies: ", analytic_local_energies)
+    print("Energies: ", Energies)
+    print("Variances: ", Variances)
+    print("time_stats: ", time_stats)
+
+    VMC_obj = VMC(N_particles, alpha, beta, a)
+    (
+        alpha_values,
+        analytic_local_energies,
+        Energies,
+        Variances,
+        time_stats,
+    ) = VMC_obj.MC_Sampling(
+        N_cycles,
+        StepSize,
+        MaxVariations,
+        Dimension,
+        KE=True,
+        Importance_sampling=True,
     )
     print("alpha_values: ", alpha_values)
     print("analytic_local_energies: ", analytic_local_energies)
--- Project1/code/results.py
+++ Project1/code/results.py
@@ -3,16 +3,16 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-# Task 1b
 alpha = 0.1
 N_particles = [1, 10, 100]
 beta = 1
 a = 0  # No interaction
-N_cycles = 1
+N_cycles = 1000
 StepSize = 1
 MaxVariations = 10
 Dimension = [1, 2, 3]
 
+""" Task 1b
 for N in N_particles:
     for dim in Dimension:
         print(f"N_particles = {N}, dim = {dim}")
@@ -35,11 +35,72 @@ for N in N_particles:
         plt.plot(alpha_values, Energies["kinetic_energy"], label=f"VMC KE {dim}D: time = {np.mean(time_stats['avarage_time_KE']):.2e}", linestyle="--")
 
         
-    plt.title(f"Energy for N={N}")
+    plt.title(f"Energy for N={N}, dt = {StepSize}")
     plt.xlabel("alpha")
     plt.ylabel("Energy")
     plt.legend()
     plt.savefig(f"fig/Energy_N{N}.png")
     plt.show()
+#"""
+
+""" Task 1c
+StepSizes = [0.1, 0.5, 1, 2, 5]
+N = 10
+
+for StepSize in StepSizes:
+    for dim in Dimension:
+        print(f"N_particles = {N}, dim = {dim}")
+        VMC_obj = VMC(N, alpha, beta, a)
+        (
+            alpha_values,
+            analytic_local_energies,
+            Energies,
+            Variances,
+            time_stats,
+        ) = VMC_obj.MC_Sampling(N_cycles, StepSize, MaxVariations, dim, KE=True, Importance_sampling = True)
+        if dim == 1:
+            plt.plot(
+                alpha_values,
+                analytic_local_energies,
+                label=f"Exact EL N = {N}",
+                linestyle="-.",
+            )
+        plt.plot(alpha_values, Energies["local_energy"], label=f"VMC EL {dim}D: time = {np.mean(time_stats['avarage_time_EL']):.2e}")
+        plt.plot(alpha_values, Energies["kinetic_energy"], label=f"VMC KE {dim}D: time = {np.mean(time_stats['avarage_time_KE']):.2e}", linestyle="--")
+
+        
+    plt.title(f"Energy for N={N}, dt = {StepSize}")
+    plt.xlabel("alpha")
+    plt.ylabel("Energy")
+    plt.legend()
+    plt.savefig(f"fig/IS_Energy_dt{StepSize}.png")
+    plt.show()
+#"""
+
+#""" Task 1d
+
+N = 10
+StepSize = 0.5
+alpha = 1
+
+VMC_obj = VMC(N, alpha, beta, a)
+(
+    alpha_values,
+    analytic_local_energies,
+    Energies,
+    Variances,
+    time_stats,
+) = VMC_obj.MC_Sampling(N_cycles, StepSize, MaxVariations, 3, KE=True,
+                        Importance_sampling = True, lr = 0.00001)
+
+variation = np.arange(0, MaxVariations)
+
+plt.plot(variation, alpha_values)
+plt.title(f"Alpha variations using GD for N = {N}, dt = {StepSize}, dim = 3, lr = 0.01")
+plt.xlabel("Variation cycle")
+plt.ylabel("alpha")
+plt.savefig(f"fig/GD_alpha_variation.png")
+plt.show()
 
 
+#"""
